712006a99d19910cc0b498e87c5ba59486b79dc9
Jakub Tomczuk <jakub.tmczk@gmail.com>
2016-04-25 21:28:33 +0200

    Done

---
+++ GK6/src/gk6/Histogram.java
@@ -0,0 +1,86 @@
+package gk6;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Paint;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.DefaultDrawingSupplier;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYBarPainter;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.data.statistics.HistogramDataset;
+
+
+public class Histogram {
+
+    private static final int BINS = 256;
+    private  static BufferedImage image ;
+    
+ 
+
+    private ChartPanel createChartPanel() {
+        // dataset
+        HistogramDataset dataset = new HistogramDataset();
+        Raster raster = image.getRaster();
+        final int w = image.getWidth();
+        final int h = image.getHeight();
+        double[] r = new double[w * h];
+        r = raster.getSamples(0, 0, w, h, 0, r);
+        dataset.addSeries("Red", r, BINS);
+        r = raster.getSamples(0, 0, w, h, 1, r);
+        dataset.addSeries("Green", r, BINS);
+        r = raster.getSamples(0, 0, w, h, 2, r);
+        dataset.addSeries("Blue", r, BINS);
+        // chart
+        JFreeChart chart = ChartFactory.createHistogram("Histogram", "Value",
+            "Count", dataset, PlotOrientation.VERTICAL, true, true, false);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
+        renderer.setBarPainter(new StandardXYBarPainter());
+        // translucent red, green & blue
+        Paint[] paintArray = {
+            new Color(0x80ff0000, true),
+            new Color(0x8000ff00, true),
+            new Color(0x800000ff, true)
+        };
+        plot.setDrawingSupplier(new DefaultDrawingSupplier(
+            paintArray,
+            DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_STROKE_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE));
+        ChartPanel panel = new ChartPanel(chart);
+        panel.setMouseWheelEnabled(true);
+        return panel;
+    }
+
+    private void display() {
+        JFrame f = new JFrame("Histogram");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(createChartPanel());
+        f.add(new JLabel(new ImageIcon(image)), BorderLayout.WEST);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ppm k  = new ppm();
+        k.load("etud.ppm");
+        image = k.ppmi;
+        EventQueue.invokeLater(() -> {
+            new Histogram().display();
+        });
+    }
+}
